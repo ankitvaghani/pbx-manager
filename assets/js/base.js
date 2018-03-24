34e6ed21ca811a937c68ecb33397123e235e24d4
ankit vaghani <ankitv.lintel@gmail.com>
2018-03-24 19:34:23 +0530

    Phones page image uploading contd...

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,14 +1,87 @@
  $(document).ready(function(){
  
-     // //enabled and disabled toggle
-     // $(function() {
-     //     $('#toggle-two').bootstrapToggle({
-     //         on: 'Enabled',
-     //         off: 'Disabled'
-     //     });
-     // });
+
 //End document.ready
 });
+var addNewPhoneApp = angular.module('myAddNewPhoneApp', ['ui.toggle']);
+      addNewPhoneApp.controller('myAddNewPhoneCtrl', function($scope) {
+
+        $scope.uploadFile = function(element) {
+        alert();
+    $scope.data.my_profile_file = element;
+    console.log($scope.data);
+  }
+
+//$('#voicemail_to_email').change(function() {
+  if($scope.showHideEmailAddress){
+       //  if (this.checked) {
+             $('#email_id').fadeIn('slow');
+              //$('#emailid').removeAttr('disabled');
+       //  } else {
+           //  $('#email_id').fadeOut('slow');
+              //$('#emailid').attr('disabled','');
+           //  $('#emailid').val('');
+       //  }
+       }
+//    });
+       $scope.addNewPhone = function() {
+           var post_data = $scope.data;
+
+           //console.log($scope.myLoginForm.$valid);
+            if (!$scope.myNewPhoneForm.$valid) {
+                 $('.alert-danger').show();
+                console.log('error');
+                //$("#divError").addClass("has-error");
+
+                return false;
+            }
+            else
+            {
+                //console.log( 'serialize: ' + $( '#myNewPhoneForm' ).serialize() );
+
+                $('.alert-danger').hide();
+                 //var file = $scope.my_profile_file;
+                 //console.log('file:', file);
+                //console.log('post data: ' , post_data);
+
+
+
+                // $("#divError").removeClass("has-error");
+                // var username = $scope.data.username;
+                // var password = $scope.data.passwd;
+                // if (username == "admin" && password == "admin")
+                // {
+                //      window.location.href = "phones.html";
+                // } else {
+                //     //alert('invalid username and password');
+                //     $('#loginError').show();
+                // }
+            }
+        }
+    });
+
+ addNewPhoneApp.directive('validFile',[function() {
+  return {
+    require : 'ngModel',
+    scope : {format: '@', upload : '&upload'},
+    link : function(scope, el, attrs, ngModel) {
+      // change event is fired when file is selected
+      el.bind('change', function(event) {
+        console.log(event.target.files[0]);
+        scope.upload({file:event.target.files[0]});
+        scope.$apply(function() {
+          ngModel.$setViewValue(el.val());
+          ngModel.$render();
+        });
+      })
+    }
+  }
+}]);
+
+    // choose file from links
+         $("#profile_upload_link").on('click', function(){
+             $("#my_profile_file:hidden").trigger('click');
+         });
 //Numeric validation
 function isNumericValidation(evt){
    // return (/\d/.test(String.fromCharCode(event.which) )) 
@@ -18,44 +91,26 @@ function isNumericValidation(evt){
         return false;
     return true;
 }
-  // //call_forwarding hide and show
-  
-  //    $('#call_forwarding').change(function() {
-  //        if (this.checked) {
-  //            $('#number_forwarding').fadeIn('slow');  
-  //           // $('#forwarding_number').attr('required', 'required');             
-  //           // $('#number_forwarding').attr("ng-class", "{ 'has-error' : myNewPhoneForm.forwarding_number.$invalid && !myNewPhoneForm.forwarding_number.$pristine }");
-  //            $('#forwarding_number').removeAttr('disabled');
-            
-  //           // console.log($('#call_forwarding').val());
-  //        } else {
-  //            $('#forwarding_number').removeAttr('required');
-  //            $('#number_forwarding').removeAttr('ng-class');
-  //            $('#number_forwarding').fadeOut('slow');
-  //            $('#forwarding_number').val('');
-  //            $('#forwarding_number').attr('disabled','');
-            
-  //            //console.log($('#call_forwarding').val());
-  //        }
-  //    });
-     
-  // //voicemail_to_email hide and show
-     // $('#voicemail_to_email').change(function() {
-     //     if (this.checked) {
-     //         $('#email_id').fadeIn('slow');
-     //          //$('#emailid').removeAttr('disabled');
-     //     } else {
-     //         $('#email_id').fadeOut('slow');
-     //          //$('#emailid').attr('disabled','');
-     //         $('#emailid').val('');
-     //     }
-     // });
-  //    //Choose profile picture
-     $(function(){
-         $("#profile_upload_link").on('click', function(){
-             $("#my_profile_file:hidden").trigger('click');
-         });
-     });
+
+
+// view file from choose links.
+     function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#profile_picture')
+                        .attr('src', e.target.result)
+                        .width(100)
+                        .height(80);
+                };
+
+                reader.readAsDataURL(input.files[0]);
+                console.log(reader);
+            }
+        }
+
+
 // $( "#myNewPhoneForm" ).on( "submit", function() {
 //          console.log( $( this ).serialize() );
 //  });
